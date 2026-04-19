a70d781904c59ebbc1bb6adbbeee870c29025935
Dasari Vamsi <44261806+darkbloodprince@users.noreply.github.com>
2026-04-19 14:26:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+function updateTime() {
+  const now = new Date();
+
+  // Convert to readable string
+  const dateTime = now.toLocaleString();
+
+  document.getElementById("timer").textContent = dateTime;
+}
+
+// Run immediately
+updateTime();
+
+// Update every second
+setInterval(updateTime, 1000);
